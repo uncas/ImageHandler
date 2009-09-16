33e6d45ac8f5ab0f1818822389f998b7e37fdad7
Ole Lynge Soerensen <olelynge@gmail.com>
2009-09-16 19:48:44 +0000

    UncasImageHandler: Moved resize module to Uncas.Drawing and referenced that instead.
    git-svn-id: http://localhost:81/svn/dev/projects/UncasImageHandler/trunk@377 90d2b0b2-0e11-ba47-89f1-85e69862abc4

--- src/UncasImageHandler/FormMain.cs
+++ src/UncasImageHandler/FormMain.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
-using Uncas.Drawing;
+using Uncas.Drawing.ImageResizing;
 
 namespace UncasImageHandler
 {
@@ -23,7 +20,7 @@ namespace UncasImageHandler
 
         #region Private fields and properties
 
-        private ImageHandler ih = new ImageHandler();
+        private ResizeImages ri = new ResizeImages();
         private const string ResizeText = "Gem";
         private const string CancelText = "Afbryd";
 
@@ -52,14 +49,6 @@ namespace UncasImageHandler
                 += new EventHandler(btnClear_Click);
             btnSaveResizedImages.Click
                 += new EventHandler(btnSaveResizedImages_Click);
-            resizeWorker.DoWork
-                += new DoWorkEventHandler(resizeWorker_DoWork);
-            resizeWorker.ProgressChanged
-                += new ProgressChangedEventHandler
-                    (resizeWorker_ProgressChanged);
-            resizeWorker.RunWorkerCompleted
-                += new RunWorkerCompletedEventHandler
-                    (resizeWorker_RunWorkerCompleted);
             #endregion
 
             #region Sets folders on file and folder dialogs
@@ -104,6 +93,52 @@ namespace UncasImageHandler
             ChangeSourceMode();
 
             SetMaxImageSize();
+
+            ri.ResizeProgressChanged
+                += new EventHandler<ResizeProgressEventArgs>
+                    (ri_ProgressChanged);
+
+            ri.ResizeCompleted
+                += new EventHandler<ResizeCompletedEventArgs>
+                    (ri_ResizeCompleted);
+
+            ri.ResizeFailed 
+                += new EventHandler<ResizeFailedEventArgs>
+                    (ri_ResizeFailed);
+        }
+
+        void ri_ResizeFailed
+            (object sender
+            , ResizeFailedEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString());
+        }
+
+        void ri_ResizeCompleted
+            (object sender
+            , ResizeCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Arbejdet blev afbrudt");
+            }
+            else
+            {
+                lblNumberCompleted.Text = e.ProcessedImages.ToString();
+                progressBar1.Value = 100;
+                MessageBox.Show("Billederne blev gemt");
+            }
+            gbSourceMode.Enabled = true;
+            gbOutputFolder.Enabled = true;
+            gbMaxSize.Enabled = true;
+            btnSaveResizedImages.Text = ResizeText;
+            Process.Start(lblOutputFolder.Text);
+        }
+
+        void ri_ProgressChanged(object sender, ResizeProgressEventArgs e)
+        {
+            lblNumberCompleted.Text = e.ProcessedImages.ToString();
+            progressBar1.Value = e.ProcessedImages.Percentage;
         }
 
         #region Handling user choices
@@ -217,7 +252,7 @@ namespace UncasImageHandler
             gbOutputFolder.Enabled = false;
             gbMaxSize.Enabled = false;
             #endregion
-            DoResizeWorkAsync
+            ri.DoResizeWorkAsync
                 (lblOutputFolder.Text
                 , rbChooseFiles.Checked
                 , rbChooseFolder.Checked
@@ -255,293 +290,6 @@ namespace UncasImageHandler
         }
 
         #endregion
-
-        #region Resizing the chosen images
-
-        #region Running in background thread
-
-        private class ImageToResize
-        {
-            public string OriginalImagePath { get; set; }
-            public string ResizedImagePath { get; set; }
-        }
-
-        private class SelectedImagesInfo
-        {
-            public int MaxImageSize { get; set; }
-            public List<ImageToResize> ImagesToResize { get; set; }
-        }
-
-        private class ProcessedImagesInfo
-        {
-            public int TotalNumberOfImages { get; set; }
-            public int ResizedNumberOfImages { get; set; }
-
-            public override string ToString()
-            {
-                return string.Format("{0}/{1}"
-                    , this.ResizedNumberOfImages
-                    , this.TotalNumberOfImages);
-            }
-        }
-
-        private void DoResizeWorkAsync
-            (string baseOutputFolder
-            , bool chooseFiles
-            , bool chooseFolder
-            , string baseInputFolder
-            , bool includeSubFolders
-            , IEnumerable filePaths
-            , int maxImageSize
-            )
-        {
-            #region Getting the list of images
-            List<ImageToResize> imagesToResize
-                = new List<ImageToResize>();
-            if (chooseFiles)
-            {
-                imagesToResize
-                    = GetSelectedImages
-                    (baseOutputFolder
-                    , filePaths);
-            }
-            else if (chooseFolder)
-            {
-                DirectoryInfo diBase
-                    = new DirectoryInfo(baseInputFolder);
-                try
-                {
-                    GetImagesInSelectedFolder
-                        (ref imagesToResize
-                        , diBase.Name
-                        , baseOutputFolder
-                        , diBase
-                        , includeSubFolders);
-                }
-                catch (Exception ex)
-                {
-                    HandleException(ex);
-                }
-            }
-            #endregion
-            #region Resizing the images
-            SelectedImagesInfo sfi = new SelectedImagesInfo
-            {
-                ImagesToResize = imagesToResize,
-                MaxImageSize = maxImageSize
-            };
-            resizeWorker.RunWorkerAsync(sfi);
-            #endregion
-        }
-
-        void resizeWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            SelectedImagesInfo sfi = (SelectedImagesInfo)e.Argument;
-            int filesCompleted = 0;
-            ProcessedImagesInfo pif = new ProcessedImagesInfo
-            {
-                TotalNumberOfImages = sfi.ImagesToResize.Count,
-                ResizedNumberOfImages = filesCompleted
-            };
-            foreach (ImageToResize itr in sfi.ImagesToResize)
-            {
-                pif.ResizedNumberOfImages = filesCompleted;
-                int percentage = (int)((100d * filesCompleted)
-                    / (1d * sfi.ImagesToResize.Count));
-                resizeWorker.ReportProgress(percentage, pif);
-                if (resizeWorker.CancellationPending)
-                {
-                    e.Cancel = true;
-                    break;
-                }
-                ResizeImage(itr.OriginalImagePath
-                    , itr.ResizedImagePath
-                    , sfi.MaxImageSize);
-                filesCompleted++;
-            }
-            pif.ResizedNumberOfImages = filesCompleted;
-            e.Result = pif;
-        }
-
-        // TODO: Create event in the new class to pass this on:
-        void resizeWorker_ProgressChanged
-            (object sender
-            , ProgressChangedEventArgs e)
-        {
-            ProcessedImagesInfo pif
-                = (ProcessedImagesInfo)e.UserState;
-            lblNumberCompleted.Text = pif.ToString();
-            progressBar1.Value = e.ProgressPercentage;
-        }
-
-        // TODO: Create event in the new class to pass this on:
-        void resizeWorker_RunWorkerCompleted
-            (object sender
-            , RunWorkerCompletedEventArgs e)
-        {
-            if (e.Cancelled)
-            {
-                MessageBox.Show("Arbejdet blev afbrudt");
-            }
-            else
-            {
-                ProcessedImagesInfo pif
-                    = (ProcessedImagesInfo)e.Result;
-                lblNumberCompleted.Text = pif.ToString();
-                progressBar1.Value = 100;
-                MessageBox.Show("Billederne blev gemt");
-            }
-            gbSourceMode.Enabled = true;
-            gbOutputFolder.Enabled = true;
-            gbMaxSize.Enabled = true;
-            btnSaveResizedImages.Text = ResizeText;
-            Process.Start(lblOutputFolder.Text);
-        }
-
-        #endregion
-
-        #region The actual resizing methods
-
-        private List<ImageToResize> GetSelectedImages
-            (string baseOutputFolder
-            , IEnumerable filePaths)
-        {
-            List<ImageToResize> imagesToResize
-                = new List<ImageToResize>();
-            foreach (string filePath in filePaths)
-            {
-                FileInfo fi = new FileInfo(filePath);
-                string smallerFilePath =
-                    Path.Combine(baseOutputFolder, fi.Name);
-                imagesToResize.Add(new ImageToResize
-                {
-                    OriginalImagePath = filePath,
-                    ResizedImagePath = smallerFilePath
-                });
-            }
-            return imagesToResize;
-        }
-
-        private void GetImagesInSelectedFolder
-            (ref List<ImageToResize> imagesToResize
-            , string relativeInputFolderPath
-            , string baseOutputFolder
-            , DirectoryInfo di
-            , bool includeSubFolders)
-        {
-            string inputFolderPath = di.FullName;
-            string outputFolderPath
-                = Path.Combine(baseOutputFolder
-                , relativeInputFolderPath);
-            DirectoryInfo diOutput
-                = new DirectoryInfo(outputFolderPath);
-            // Getting images in this folder
-            GetFilesByExtension
-                (imagesToResize
-                , di
-                , outputFolderPath
-                , diOutput
-                , "*.jpg");
-            GetFilesByExtension
-                (imagesToResize
-                , di
-                , outputFolderPath
-                , diOutput
-                , "*.jpeg");
-            GetFilesByExtension
-                (imagesToResize
-                , di
-                , outputFolderPath
-                , diOutput
-                , "*.bmp");
-            GetFilesByExtension
-                (imagesToResize
-                , di
-                , outputFolderPath
-                , diOutput
-                , "*.png");
-            GetFilesByExtension
-                (imagesToResize
-                , di
-                , outputFolderPath
-                , diOutput
-                , "*.gif");
-            if (includeSubFolders)
-            {
-                // Resizing images in subfolders
-                foreach (DirectoryInfo diChild in di.GetDirectories())
-                {
-                    string childRelativePath
-                        = Path.Combine
-                        (relativeInputFolderPath
-                        , diChild.Name);
-                    GetImagesInSelectedFolder
-                        (ref imagesToResize
-                        , childRelativePath
-                        , baseOutputFolder
-                        , diChild
-                        , includeSubFolders);
-                }
-            }
-        }
-
-        private void GetFilesByExtension
-            (List<ImageToResize> imagesToResize
-            , DirectoryInfo di
-            , string outputFolderPath
-            , DirectoryInfo diOutput
-            , string extension)
-        {
-            foreach (FileInfo fi in di.GetFiles(extension))
-            {
-                if (!diOutput.Exists)
-                    diOutput.Create();
-                string originalImagePath = fi.FullName;
-                string resizedImagePath
-                    = Path.Combine(outputFolderPath, fi.Name);
-                imagesToResize.Add(new ImageToResize
-                {
-                    OriginalImagePath = originalImagePath,
-                    ResizedImagePath = resizedImagePath
-                });
-            }
-        }
-
-        private void ResizeImage
-            (string originalImagePath
-            , string resizedImagePath
-            , int maxImageSize)
-        {
-            try
-            {
-                if (File.Exists(originalImagePath)
-                    && !File.Exists(resizedImagePath))
-                {
-                    byte[] originalBytes
-                        = File.ReadAllBytes(originalImagePath);
-                    byte[] resizedBytes
-                        = ih.GetThumbnail(originalBytes, maxImageSize);
-                    originalBytes = null;
-                    File.WriteAllBytes(resizedImagePath, resizedBytes);
-                    resizedBytes = null;
-                    GC.WaitForPendingFinalizers();
-                }
-            }
-            catch (Exception ex)
-            {
-                HandleException(ex);
-            }
-        }
-
-        #endregion
-
-        #endregion
-
-        // TODO: Create event in the new class to pass this on:
-        private void HandleException(Exception ex)
-        {
-            MessageBox.Show(ex.ToString());
-        }
 
         #endregion
     }
